7f0c49609777f52cfac761c1d91aa2ae83907d2e
MichaelOppong <OppongM@gmail.com>
2024-05-10 02:16:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Make the rows match
+
+// 1. Set the styling of `example1` to a background color of black.
+
+// 2. Set the styling of `pink` to a background color of pink.
+
+// 3. Set the styling of `green` to a background color of green.
+
+// 4. Set the styling of `blue` to a background color of blue.
+
+// 5. Set the styling of `lightGreen` to a background color of lightGreen.
+
+// 6. Set the styling of `row1` to have a font color of white.
